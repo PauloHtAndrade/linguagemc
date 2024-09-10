c4b66f1a3ec3708be94b73a4363a337d3fbb9bb9
Paulo <paulothandrade@gmail.com>
2024-09-10 14:24:14 +0000

    Inicio do estudo em C

---
+++ soma_dois.c
@@ -0,0 +1,12 @@
+    #include <stdio.h>
+
+    int main(){
+
+        int n1,n2,resultado;
+        n1 = 5;
+        n2 = 10;
+        resultado = n1 + n2;
+        printf("resultado da soma de %d com %d é igual %d \n" , n1, n2, resultado);
+        retorn 0;
+
+    }
