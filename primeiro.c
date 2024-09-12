54bfe95b5744547348d908383de603a8027420fd
Paulo <paulothandrade@gmail.com>
2024-09-12 12:39:37 +0000

    Criação dos arquivos notas_alunos2 e rodizio_if

---
+++ primeiro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (){
+
+    int j;
+    printf("Digite um valor inteiro e ENTER\n");
+    // declara var. j inteira
+    // mostra na tela
+    // ler j, NAO digitar NADA entre %d
+    // mostra na tela valor de j lido
+    scanf("%d", &j);
+    
+    printf("Valor de j e ' %d\n", j);
+
+    return 0;
+//fim
+
+
+
+
+}
